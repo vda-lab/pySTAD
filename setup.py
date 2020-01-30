da2e0009a808bb8528c4d473fcde6dbcfb146ad9
Jan Aerts <jan.aerts@gmail.com>
2020-01-30 20:18:50 +0100

    Added files necessary for pip package

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stad",
+    version="2.0.0",
+    author="Jan Aerts",
+    author_email="jan.aerts@uhasselt.be",
+    description="Dimensionality reduction through Simplified Topological Abstraction of Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vda-lab/pySTAD",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering"
+    ],
+    python_requires='>=3',
+)
